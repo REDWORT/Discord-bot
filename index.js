1995534f25849038416fd9d83e64900a5244efa7
REDWORT <52329053+REDWORT@users.noreply.github.com>
2019-06-28 18:21:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-//const Discord = require('discord.js');
-//const bot = new Discord.Client();
 
-const commando =  require('discord.js-commando');
-const bot = new commando.Client();
-
-bot.registry.registerGroup('random','Random')
-bot.registry.registerDefaults();
-bot.registry.registerCommandsIn(__dirname + "/commands")
-
-bot.login('NTg5NTA5NjE5MTAxOTkwOTUz.XQXaWQ.FHj18_fyMwWJUKRM-UU9h2H3g5I');
